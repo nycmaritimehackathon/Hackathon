607495710daa1e1f2c4caa3501490ebc7a452c36
zhhjoseph <zhhjoseph@gmail.com>
2019-03-20 14:49:03 -0400

    added basic css

--- client/components/root.js
+++ client/components/root.js
@@ -1,10 +1,11 @@
 import React from "react";
+import '../../public/stylesheet.css'
 // import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export default class Root extends React.Component {
   render() {
     return (
-      <div>Test DIV</div>
+      <div class = "startingtext">Start your project!</div>
     )
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,13 @@ module.exports = {
         use: {
           loader: 'babel-loader'
         }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ]
       }
     ]
   }
